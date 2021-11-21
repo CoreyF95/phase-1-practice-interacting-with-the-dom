3ddfbb3e9e0c8c425a8d68061e50ad0e6acd2d4c
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-21 14:46:06 -0600

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,85 @@
+const counter = document.getElementById("counter")
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const heart = document.getElementById("heart");
+const pause = document.getElementById("pause");
+const comment = document.getElementById("comment-input");
+const submit = document.getElementById("submit");
+const likes = document.querySelector("ul.likes");
+const list = document.getElementById("list");
+let seconds = 0;
+let heartClicks = 0;
+
+let heartZero = () => {
+    heartClicks = 0;
+}
+
+function timer() {
+    if (submit.disabled === false) {
+        seconds += 1;
+        counter.textContent = seconds;
+    } else {
+        seconds += 0;
+        counter.textContent = seconds;
+    }
+}
+
+const heartReset = setInterval(heartZero, 1000);
+const timeElapsed = setInterval(timer, 1000);
+
+
+
+let heartCounter = 
+    heart.addEventListener("click", () => {
+        return heartClicks += 1;
+    });
+
+
+
+minus.addEventListener("click", () => {
+    counter.textContent = seconds -= 1;
+})
+
+
+
+plus.addEventListener("click", () => {
+    counter.textContent = seconds += 1;
+})
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newComment = document.createElement("p");
+    document.getElementById("list").appendChild(newComment);
+    newComment.textContent = comment.value;
+    comment.value = "";
+    
+
+})
+
+pause.addEventListener("click", () => {
+    if (pause.textContent === "pause") {
+        pause.textContent = "resume";
+        plus.disabled = true;
+        minus.disabled = true;
+        heart.disabled = true;
+        submit.disabled = true;
+        counter.textContent = seconds.valueOf();
+    } else {
+        pause.textContent = "pause";
+        plus.disabled = false;
+        minus.disabled = false;
+        heart.disabled = false;
+        submit.disabled = false;
+        timer();
+    }
+
+    // TAKES 2 CLICKS TO START WORKING
+})
+
+heart.addEventListener("click", () => {
+    let like = document.createElement("li");
+    likes.appendChild(like);
+    like.textContent = `${seconds} has been liked ${heartClicks} times`;
+
+    // APPENDS EVERY CLICK
+})
